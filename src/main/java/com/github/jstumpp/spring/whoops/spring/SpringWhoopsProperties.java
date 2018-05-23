e4eeda4204f8a3bac324a0f4e9bc59fb1008d02a
Filip Procházka <filip@prochazka.su>
2018-05-24 00:48:19 +0200

    fix SpringWhoopsProperties

--- src/main/java/com/github/jstumpp/spring/whoops/spring/SpringWhoopsProperties.java
+++ src/main/java/com/github/jstumpp/spring/whoops/spring/SpringWhoopsProperties.java
@@ -1,12 +1,6 @@
 package com.github.jstumpp.spring.whoops.spring;
 
-import java.util.List;
-
-import javax.validation.constraints.NotNull;
-
-import org.springframework.boot.context.embedded.Ssl.ClientAuth;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.core.io.Resource;
 import org.springframework.validation.annotation.Validated;
 
 /**
@@ -23,15 +17,15 @@ import org.springframework.validation.annotation.Validated;
 @ConfigurationProperties(prefix = "whoops")
 public class SpringWhoopsProperties {
 
-    private boolean m_enabled = true;
+    private boolean enabled = true;
 
     /**
      * @return Whether any {@code @Bean}s should be injected at all.
      */
     public boolean isEnabled() {
-        return m_enabled;
+        return enabled;
     }
     public void setEnabled(boolean enabled) {
-        m_enabled = enabled;
+        this.enabled = enabled;
     }
 }
